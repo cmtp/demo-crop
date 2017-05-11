d29432d9d19b1250e07476789b7e41d6cc8c2850
ctola <christian.tola@dharbor.com>
2017-05-11 11:24:20 -0400

    add crop module and controller

--- app/crop/crop.controller.js
+++ app/crop/crop.controller.js
@@ -5,8 +5,8 @@
         .module('app.crop')
         .controller('CropController', CropController);
 
-    CropController.inject = ['$log'];
-    function CropController($log) {
+    CropController.inject = ['$log', '$scope', '$timeout', 'Upload'];
+    function CropController($log, $scope, $timeout, Upload) {
         var vm = this;
         
 
@@ -14,6 +14,24 @@
 
         ////////////////
 
-        function activate() { }
+        function activate() {
+            $scope.upload = function (dataUrl, name) {
+                Upload.upload({
+                    url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                    data: {
+                        file: Upload.dataUrltoBlob(dataUrl, name)
+                    },
+                }).then(function (response) {
+                    $timeout(function () {
+                        $scope.result = response.data;
+                    });
+                }, function (response) {
+                    if (response.status > 0) $scope.errorMsg = response.status 
+                        + ': ' + response.data;
+                }, function (evt) {
+                    $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
+                });
+            }
+        }
     }
 })();
